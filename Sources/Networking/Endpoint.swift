e92af59881621dfb8513bedf85a5018ce0c921af
Felipe Dias Pereira <felipediaspereira@Felipes-MacBook-Pro.local>
2022-05-14 13:50:15 -0300

    Refactor to work with async await

--- Sources/Networking/Endpoint.swift
+++ Sources/Networking/Endpoint.swift
@@ -9,59 +9,73 @@ import Foundation
 // MARK: Endpoint
 
 public struct Endpoint<Kind: EndpointKind> {
-  public let path: String
-  public var method: Method = .get
-  public var queryItems = [URLQueryItem]()
-  public var bodyParameters = [String: Any]()
+    public let path: String
+    public var method: Method = .get
+    public var queryItems = [URLQueryItem]()
+    public var bodyParameters = [String: Any]()
 
-  public enum Method: String {
-    case post = "POST"
-    case get = "GET"
-    case put = "PUT"
-    case patch = "PATCH"
-    case delete = "DELETE"
-  }
+    public enum Method: String {
+        case post = "POST"
+        case get = "GET"
+        case put = "PUT"
+        case patch = "PATCH"
+        case delete = "DELETE"
+    }
 
-  public init(path: String,
-              method: Endpoint<Kind>.Method = .get,
-              queryItems: [URLQueryItem] = [URLQueryItem](),
-              bodyParameters: [String : Any] = [String: Any]()) {
-    self.path = path
-    self.method = method
-    self.queryItems = queryItems
-    self.bodyParameters = bodyParameters
-  }
+    public init(path: String,
+                method: Endpoint<Kind>.Method = .get,
+                queryItems: [URLQueryItem] = [URLQueryItem](),
+                bodyParameters: [String : Any] = [String: Any]()) {
+        self.path = path
+        self.method = method
+        self.queryItems = queryItems
+        self.bodyParameters = bodyParameters
+    }
+}
+
+// MARK: - Erros
+extension Endpoint {
+    enum Errors: LocalizedError {
+        case URLMalformated
+    }
 }
 
 extension Endpoint {
-  func makeRequest(host: String, with data: Kind.RequestData) -> URLRequest? {
-    var components = URLComponents()
-    components.scheme = "https"
-    components.host = host
-    components.path = path
-    components.queryItems = queryItems.isEmpty ? nil : queryItems
+    func makeRequest(host: String, with data: Kind.RequestData) -> URLRequest? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = host
+        components.path = path
+        components.queryItems = queryItems.isEmpty ? nil : queryItems
 
-    guard let url = components.url else {
-      return nil
+        guard let url = components.url else {
+            return nil
+        }
+
+        var request = URLRequest(url: url)
+        configRequest(&request)
+        Kind.prepare(&request, with: data)
+        return request
     }
 
-    var request = URLRequest(url: url)
-    configRequest(&request)
-    Kind.prepare(&request, with: data)
-    return request
-  }
+    func makeRequest(host: String, with data: Kind.RequestData) throws -> URLRequest {
+        guard let request = makeRequest(host: host, with: data) else {
+            throw Errors.URLMalformated
+        }
+        return request
+    }
 }
 
 private extension Endpoint {
-  var shouldAddBodyToRequest: Bool {
-    return method == .post || method == .put || method == .patch
-  }
-
-  func configRequest(_ request: inout URLRequest) {
-    request.httpMethod = method.rawValue
-    if shouldAddBodyToRequest,
-       let httpBody = try? JSONSerialization.data(withJSONObject: bodyParameters, options: []) {
-      request.httpBody = httpBody
+    var shouldAddBodyToRequest: Bool {
+        return method == .post || method == .put || method == .patch
     }
-  }
+
+    func configRequest(_ request: inout URLRequest) {
+        request.httpMethod = method.rawValue
+        if shouldAddBodyToRequest,
+           let httpBody = try? JSONSerialization.data(withJSONObject: bodyParameters, options: []) {
+            request.httpBody = httpBody
+        }
+    }
 }
--- Tests/NetworkingTests/NetworkingTests.swift
+++ Tests/NetworkingTests/NetworkingTests.swift
@@ -1,197 +1,179 @@
-//  swiftlint:disable force_try
+
 import XCTest
 import Combine
-import Mocker
 @testable import Networking
 
-final class NetworkingTests: XCTestCase {
-  struct MockResponse: Decodable, Equatable {
-    var title: String
-    var date: Date?
-  }
-
-  var sut: NetworkRequestable!
-
-  override func setUpWithError() throws {
-    sut = NetworkService(host: "testing.com")
-  }
-
-  func testRequestObjectResponseGetSuccess() throws {
-    let originalURL = URL(string: "https://testing.com/object/response/success")!
-    let data = try! JSONSerialization.data(withJSONObject: ["title": "Mocker"], options: .fragmentsAllowed)
-    let mock = Mock(url: originalURL,
-                    dataType: .json,
-                    statusCode: 200,
-                    data: [.get: data])
-    mock.register()
-
-    let endpoint = Endpoint<EndpointKinds.Public>(path: "/object/response/success")
-    let exp = XCTestExpectation(description: #function)
-    sut.request(for: endpoint, using: (), decoder: .init()) { (result: Result<MockResponse, NetworkError>) in
-      switch result {
-      case let .success(object):
-        XCTAssertEqual(object.title, "Mocker")
-      case let .failure(error):
-        XCTFail(error.localizedDescription)
-      }
-      exp.fulfill()
+open class NetworkingTests: XCTestCase {
+    struct MockResponse: Decodable, Equatable {
+        var title: String
+        var date: Date?
     }
-    wait(for: [exp], timeout: 2)
-  }
 
-  func testRequestObjectResponsePostSuccess() throws {
-    let originalURL = URL(string: "https://testing.com/object/response/success")!
-    let data = try! JSONSerialization.data(withJSONObject: ["title": "Mocker"], options: .fragmentsAllowed)
-    let mock = Mock(url: originalURL,
-                    dataType: .json,
-                    statusCode: 200,
-                    data: [.post: data])
-    mock.register()
+    var sut: NetworkRequestable!
+    var responseData: Data!
+    var sessionMock: URLSessionMock!
+    var decoder = JSONDecoder()
 
-    let endpoint = Endpoint<EndpointKinds.Public>(path: "/object/response/success", method: .post)
-    let exp = XCTestExpectation(description: #function)
-    sut.request(for: endpoint, using: (), decoder: .init()) { (result: Result<MockResponse, NetworkError>) in
-      switch result {
-      case let .success(object):
-        XCTAssertEqual(object.title, "Mocker")
-      case let .failure(error):
-        XCTFail(error.localizedDescription)
-      }
-      exp.fulfill()
+    open override func setUpWithError() throws {
+        sessionMock = URLSessionMock()
+        sut = NetworkService(host: "testing.com", urlSession: sessionMock)
+        responseData = try JSONSerialization.data(withJSONObject: ["title": "Mocker"], options: .fragmentsAllowed)
+        try super.setUpWithError()
     }
-    wait(for: [exp], timeout: 2)
-  }
 
-  func testRequestObjectReponseFailure() {
-    let originalURL = URL(string: "https://testing.com/to/failure")!
-    let mock = Mock(url: originalURL,
-                    dataType: .json,
-                    statusCode: 500,
-                    data: [.get: Data()],
-                    requestError: URLError(.notConnectedToInternet))
-
-    mock.register()
-
-    let endpoint = Endpoint<EndpointKinds.Public>(path: "/to/failure")
-
-    let exp = XCTestExpectation(description: #function)
-    sut.request(for: endpoint, using: (), decoder: .init()) { (result: Result<MockResponse, NetworkError>) in
-      switch result {
-      case .success:
-        XCTFail("Should return error")
-      case let .failure(error):
-        XCTAssertEqual(error, NetworkError.notConnectedToInternet)
-      }
-      exp.fulfill()
+    func createResponse(with status: HTTPStatusCode) throws -> HTTPURLResponse {
+        guard let response = HTTPURLResponse(
+            url: URL(fileURLWithPath: ""),
+            statusCode: status.rawValue,
+            httpVersion: nil,
+            headerFields: nil
+        ) else {
+            throw NetworkError.emptyData
+        }
+        return response
     }
-    wait(for: [exp], timeout: 2)
-  }
+}
 
-  func testRequestObjectResponseDataEmptyFailure() {
-    let originalURL = URL(string: "https://testing.com/to/dataempty")!
-    let endpoint = Endpoint<EndpointKinds.Public>(path: "/to/dataempty")
-    let mock = Mock(url: originalURL,
-                    dataType: .json,
-                    statusCode: 200,
-                    data: [.get: Data()])
+final class NetworkingCallback: NetworkingTests {
+    func testRequestObjectResponseGetSuccess() throws {
+        sessionMock.data = responseData
+        sessionMock.urlResponse = try createResponse(with: .success)
 
-    mock.register()
-
-    let exp = XCTestExpectation(description: #function)
-    sut.request(for: endpoint, using: (), decoder: .init()) { (result: Result<MockResponse, NetworkError>) in
-      switch result {
-      case .success:
-        XCTFail("Should return error")
-      case let .failure(error):
-        XCTAssertEqual(error, NetworkError.parse(nil))
-      }
-      exp.fulfill()
+        let endpoint = Endpoint<EndpointKinds.Public>(path: "/object/response/success")
+        let exp = XCTestExpectation(description: #function)
+        sut.request(for: endpoint, using: (), decoder: decoder) { (result: Result<MockResponse, NetworkError>) in
+            switch result {
+            case let .success(object):
+                XCTAssertEqual(object.title, "Mocker")
+            case let .failure(error):
+                XCTFail(error.localizedDescription)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
     }
-    wait(for: [exp], timeout: 2)
-  }
 
-  func testRequestObjectResponseUnauthorizedFailure() {
-    let originalURL = URL(string: "https://testing.com/to/unauthorized")!
-    let endpoint = Endpoint<EndpointKinds.Public>(path: "/to/unauthorized")
-    let mock = Mock(url: originalURL,
-                    dataType: .json,
-                    statusCode: 401,
-                    data: [.get: Data()])
+    func testRequestObjectResponsePostSuccess() throws {
+        sessionMock.data = responseData
+        sessionMock.urlResponse = try createResponse(with: .success)
 
-    mock.register()
-
-    let exp = XCTestExpectation(description: #function)
-    sut.request(for: endpoint, using: (), decoder: .init()) { (result: Result<MockResponse, NetworkError>) in
-      switch result {
-      case .success:
-        XCTFail("Should return error")
-      case let .failure(error):
-        XCTAssertEqual(error, NetworkError.serverSideError(HTTPStatusCode.unauthorized))
-      }
-      exp.fulfill()
+        let endpoint = Endpoint<EndpointKinds.Public>(path: "/object/response/success", method: .post)
+        let exp = XCTestExpectation(description: #function)
+        sut.request(for: endpoint, using: (), decoder: decoder) { (result: Result<MockResponse, NetworkError>) in
+            switch result {
+            case let .success(object):
+                XCTAssertEqual(object.title, "Mocker")
+            case let .failure(error):
+                XCTFail(error.localizedDescription)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
     }
-    wait(for: [exp], timeout: 2)
-  }
 
-  func testRequestObjectResponseEndpointFailure() {
-    let originalURL = URL(string: "https://testing.com/to/unauthorized")!
-    let endpoint = Endpoint<EndpointKinds.Public>(path: "unauthorized")
-    let mock = Mock(url: originalURL,
-                    dataType: .json,
-                    statusCode: 401,
-                    data: [.get: Data()])
+    func testRequestObjectReponseFailure() {
+        sessionMock.error = URLError(.notConnectedToInternet)
 
-    mock.register()
-
-    let exp = XCTestExpectation(description: #function)
-    sut.request(for: endpoint, using: (), decoder: .init()) { (result: Result<MockResponse, NetworkError>) in
-      switch result {
-      case .success:
-        XCTFail("Should return error")
-      case let .failure(error):
-        XCTAssertEqual(error, NetworkError.invalidEndpointError)
-      }
-      exp.fulfill()
+        let endpoint = Endpoint<EndpointKinds.Public>(path: "/to/failure")
+        let exp = XCTestExpectation(description: #function)
+        sut.request(for: endpoint, using: (), decoder: decoder) { (result: Result<MockResponse, NetworkError>) in
+            switch result {
+            case .success:
+                XCTFail("Should return error")
+            case let .failure(error):
+                XCTAssertEqual(error, NetworkError.notConnectedToInternet)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
     }
-    wait(for: [exp], timeout: 2)
-  }
 
-  func testRequestWithDateDecodingStrategy() {
-    let yyyyMMdd: DateFormatter = DateFormatter()
-    yyyyMMdd.dateFormat = "yyyy-MM-dd"
-    let jsonDecoder = JSONDecoder()
-    jsonDecoder.dateDecodingStrategy = .formatted(yyyyMMdd)
+    func testRequestObjectResponseDataEmptyFailure() throws {
+        sessionMock.data = Data()
+        sessionMock.urlResponse = try createResponse(with: .success)
 
-    let originalURL = URL(string: "https://testing.com/object/response/date")!
-    let data = try! JSONSerialization.data(withJSONObject: ["title": "Mocker",
-                                                            "date": "2020-11-05"], options: .fragmentsAllowed)
-    let mock = Mock(url: originalURL,
-                    dataType: .json,
-                    statusCode: 200,
-                    data: [.get: data])
-    mock.register()
-
-    let endpoint = Endpoint<EndpointKinds.Public>(path: "/object/response/date")
-    let exp = XCTestExpectation(description: #function)
-    sut.request(for: endpoint, using: (), decoder: jsonDecoder) { (result: Result<MockResponse, NetworkError>) in
-      switch result {
-      case let .success(object):
-        XCTAssertEqual(object.date, yyyyMMdd.date(from: "2020-11-05"))
-      case let .failure(error):
-        XCTFail(error.localizedDescription)
-      }
-      exp.fulfill()
+        let endpoint = Endpoint<EndpointKinds.Public>(path: "/to/dataempty")
+        let exp = XCTestExpectation(description: #function)
+        sut.request(for: endpoint, using: (), decoder: decoder) { (result: Result<MockResponse, NetworkError>) in
+            switch result {
+            case .success:
+                XCTFail("Should return error")
+            case let .failure(error):
+                XCTAssertEqual(error, NetworkError.parse(nil))
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
     }
-    wait(for: [exp], timeout: 2)
-  }
 
-  static var allTests = [
-    ("testRequestObjectResponseGetSuccess", testRequestObjectResponseGetSuccess),
-    ("testRequestObjectResponsePostSuccess", testRequestObjectResponsePostSuccess),
-    ("testRequestObjectReponseFailure", testRequestObjectReponseFailure),
-    ("testRequestObjectResponseDataEmptyFailure", testRequestObjectResponseDataEmptyFailure),
-    ("testRequestObjectResponseUnauthorizedFailure", testRequestObjectResponseUnauthorizedFailure),
-    ("testRequestObjectResponseEndpointFailure", testRequestObjectResponseEndpointFailure),
-    ("testRequestWithDateDecodingStrategy", testRequestWithDateDecodingStrategy),
-  ]
+    func testRequestObjectResponseUnauthorizedFailure() throws {
+        sessionMock.urlResponse = try createResponse(with: .unauthorized)
+
+        let endpoint = Endpoint<EndpointKinds.Public>(path: "/to/unauthorized")
+        let exp = XCTestExpectation(description: #function)
+        sut.request(for: endpoint, using: (), decoder: decoder) { (result: Result<MockResponse, NetworkError>) in
+            switch result {
+            case .success:
+                XCTFail("Should return error")
+            case let .failure(error):
+                XCTAssertEqual(error, NetworkError.serverSideError(HTTPStatusCode.unauthorized))
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
+    }
+
+    func testRequestObjectResponseEndpointFailure() throws {
+        let endpoint = Endpoint<EndpointKinds.Public>(path: "unauthorized")
+        let exp = XCTestExpectation(description: #function)
+        sut.request(for: endpoint, using: (), decoder: decoder) { (result: Result<MockResponse, NetworkError>) in
+            switch result {
+            case .success:
+                XCTFail("Should return error")
+            case let .failure(error):
+                XCTAssertEqual(error, NetworkError.invalidEndpointError)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
+    }
+
+    func testRequestWithDateDecodingStrategy() throws {
+        let yyyyMMdd: DateFormatter = DateFormatter()
+        yyyyMMdd.dateFormat = "yyyy-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(yyyyMMdd)
+
+        let data = try JSONSerialization.data(
+            withJSONObject: [
+                "title": "Mocker",
+                "date": "2020-11-05"
+            ],
+            options: .fragmentsAllowed
+        )
+        sessionMock.data = data
+        sessionMock.urlResponse = try createResponse(with: .success)
+
+        let endpoint = Endpoint<EndpointKinds.Public>(path: "/object/response/date")
+        let exp = XCTestExpectation(description: #function)
+        sut.request(for: endpoint, using: (), decoder: decoder) { (result: Result<MockResponse, NetworkError>) in
+            switch result {
+            case let .success(object):
+                XCTAssertEqual(object.date, yyyyMMdd.date(from: "2020-11-05"))
+            case let .failure(error):
+                XCTFail(error.localizedDescription)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 2)
+    }
+
+    static var allTests = [
+        ("testRequestObjectResponseGetSuccess", testRequestObjectResponseGetSuccess),
+        ("testRequestObjectResponsePostSuccess", testRequestObjectResponsePostSuccess),
+        ("testRequestObjectReponseFailure", testRequestObjectReponseFailure),
+        ("testRequestObjectResponseDataEmptyFailure", testRequestObjectResponseDataEmptyFailure),
+        ("testRequestObjectResponseUnauthorizedFailure", testRequestObjectResponseUnauthorizedFailure),
+        ("testRequestObjectResponseEndpointFailure", testRequestObjectResponseEndpointFailure),
+        ("testRequestWithDateDecodingStrategy", testRequestWithDateDecodingStrategy),
+    ]
 }
